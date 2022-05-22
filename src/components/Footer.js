aa50e7fb9266b1c98e3192a76ad48cb0eb677239
kuith <kuithsteam@gmail.com>
2022-05-22 19:28:56 +0200

    Primer commit

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+function Footer() {
+  return (
+    <footer className="py-5">
+    <p className="text-center mb-0">© Sanoma Educación S.L.U / Richmond</p>
+    <ul className="footer-links text-center p-0">
+      <li><a href="mailto:richmond@richmondelt.es?body=Método: HIGH ACHIEVERS ASSESSMENT EDITION (Esta información es importante, por favor no la borre). Escriba su consulta a partir de aquí." className="d-block px-2" lang="es">Contacto</a></li>
+      <li><a href="/condicionesdeuso" className="d-block px-2" lang="es" target="_blank">Condiciones de uso <span className="visually-hidden">(abre una nueva pestaña)</span></a></li>
+      <li><a href="/politicadeprivacidad" className="d-block px-2" lang="es" target="_blank">Política de privacidad <span className="visually-hidden">(abre una nueva pestaña)</span></a></li>
+    </ul>
+  </footer>
+  );
+  
+}
+
+export default Footer;
